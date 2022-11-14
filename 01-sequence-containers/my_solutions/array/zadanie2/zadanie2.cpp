59f3e608286916b01c46f81ea8d862e7feebd000
PawelN93 <paweln1993@gmail.com>
2022-11-14 20:56:13 +0100

    Array zadanie 2

---
+++ 01-sequence-containers/my_solutions/array/zadanie2/zadanie2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <array>
+
+int main()
+{
+    std::array<int, 10> tab{};
+
+    tab.fill(5);
+    tab[3] = 3;
+
+    std::array<int, 10> arr{};
+
+    arr = tab;
+    //tab.swap(arr);
+
+    std::cout << "Zawartosc 1 tablicy: " << std::endl;
+
+    for (const auto &el : tab)
+    {
+        std::cout << el << " ";
+    }
+    std::cout << "\n";
+
+    std::cout << "Zawartosc 2 tablicy: " << std::endl;
+
+    for (const auto &el : arr)
+    {
+        std::cout << el << " ";
+    }
+    std::cout << "\n";
+
+    return 0;
+}
